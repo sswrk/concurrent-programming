7b3ce6e6782b0922500ab24851e64163d9b3a881
sswrk <58403334+sswrk@users.noreply.github.com>
2020-11-04 17:22:00 +0100

    lab5 done

---
+++ lab5/src/com/company/ProducersConsumers/Consumer.java
@@ -0,0 +1,28 @@
+package com.company.ProducersConsumers;
+
+import java.util.Random;
+
+public class Consumer implements Runnable
+{
+    private Buffer buffer;
+    Random generator = new Random();
+
+    public Consumer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                buffer.take(generator.nextInt(2) + 1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
