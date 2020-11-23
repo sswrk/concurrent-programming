9fb6ee25016f58c3bc898acc1eb2414944fd4c89
sswrk <58403334+sswrk@users.noreply.github.com>
2020-11-23 17:02:17 +0100

    lab6 done

---
+++ lab6/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import com.company.ProducersConsumers.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+    private static void producersConsumers() throws InterruptedException
+    {
+        Map<Integer, Integer> hashMap = new ConcurrentHashMap<>();
+        Monitor monitor = new Monitor(15);
+        int consumerNumber = 15;
+        int producerNumber = 5;
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0 ; i < producerNumber; i++) {
+            Thread newProducer = new Thread(new Producer(monitor, hashMap));
+            newProducer.start();
+            threads.add(newProducer);
+        }
+
+        for (int i = 0; i < consumerNumber; i++) {
+            Thread newConsumer = new Thread(new Consumer(monitor, hashMap));
+            newConsumer.start();
+            threads.add(newConsumer);
+        }
+
+        for (Thread thread: threads) {
+            thread.join();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        producersConsumers();
+    }
+}
