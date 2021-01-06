32faf01878db65eb525aaa26ceb1d66fbdea44b1
sswrk <swierk.szymon99@gmail.com>
2021-01-06 21:06:10 +0100

    lab9 added

---
+++ lab9/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import org.jcsp.lang.*;
+
+/**
+ * Main program class for Producer/Consumer example.
+ * Sets up channels, creates processes then
+ * executes them in parallel, using JCSP.
+ */
+public final class Main {
+    public static void main(String[] args) {
+        new Main();
+    } // main
+
+    public Main() { // Create channel objects
+        final One2OneChannelInt[] prodChan = {Channel.one2oneInt(), Channel.one2oneInt()}; // Producers
+        final One2OneChannelInt[] consReq = {Channel.one2oneInt(), Channel.one2oneInt()}; // Consumer requests
+        final One2OneChannelInt[] consChan = {Channel.one2oneInt(), Channel.one2oneInt()}; // Consumer data
+// Create parallel construct
+        CSProcess[] procList = {new Producer(prodChan[0], 0),
+                new Producer(prodChan[1], 100),
+                new Buffer(prodChan, consReq,
+                        consChan),
+                new Consumer(consReq[0],
+                        consChan[0]),
+                new Consumer(consReq[1],
+                        consChan[1])}; // Processes
+        Parallel par = new Parallel(procList); // PAR construct
+        par.run(); // Execute processes in parallel
+    } // PCMain constructor
+} // class PCMain2
